4a67e8ead33773094df98cb7dae2e95ce6edaa0e
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-16 22:33:28 +0000

    done with solution. About to test

--- src/index.js
+++ src/index.js
@@ -25,26 +25,52 @@
 // // console.log(animal);
 // console.log(makeSound());
 
-// // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const [honda, tesla] = cars;
+
+// console.log(honda);
+
+const {
+  coloursByPopularity: hondaColour,
+  speedStats: { topSpeed: hondaTopSpeed },
+} = honda;
+const hondaTopColour = hondaColour[0];
+console.log(hondaTopColour);
+
+const {
+  coloursByPopularity: teslaColour,
+  speedStats: { topSpeed: teslaTopSpeed },
+} = tesla;
+const teslaTopColour = teslaColour;
+// // const [teslaTopSpeed, teslaTopColour] = speedStats;
+// console.log(topSpeed);
+// let hondaTopSpeed = topSpeed;
+// console.log(hondaTopSpeed);
+
+// console.log(tesla);
+// console.log(tesla.speedStats);
+// let teslaTopSpeed = tesla.speedStats;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
